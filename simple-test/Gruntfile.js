14536535dde8f7fadb87df5f6a79a9bfdb7be1e5
lishengze <lishengz.cool@hotmail.com>
2016-04-13 17:50:45 +0800

    一些修改

--- simple-test/Gruntfile.js
+++ simple-test/Gruntfile.js
@@ -14,13 +14,24 @@ module.exports = function (grunt) {
         },
         jshint: {
             options : {
-                jshintrc:'.jshintrc'               
+                jshintrc:'.jshintrc'
             },
             build:['Gruntfile.js', 'src/*.js']
+        },
+        watch: {
+          build: {
+            files: ['src/*.js', 'src/*.css'],
+            tasks: ['jshint', 'uglify']
+          },
+          options:{spawn:false}
         }
+
     });
+
+    // 载入执行任务的模块;
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // 任务注册
-    grunt.registerTask('default', ['uglify','jshint']);
+    grunt.registerTask('default', ['uglify','jshint','watch']);
 };
--- simple-test/src/test.js
+++ simple-test/src/test.js
@@ -1,9 +1,9 @@
 (function () {
     console.log('Hello Grunt');
-    
+
     function add(a, b) {
         return a + b;
     }
-    
+
     var testValue = add(1,1);
 })();
